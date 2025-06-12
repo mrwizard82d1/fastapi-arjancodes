08f1edd6c0e84ba5c2ec378975eaee54678dd544
Larry Jones <mrwizard82d1@gmail.com>
2025-06-12 17:22:07 -0500

    A query with a (single) parameter (and errors)

--- test_server.py
+++ test_server.py
@@ -2,3 +2,6 @@ import httpx
 
 print(httpx.get("http://127.0.0.1:8000/items/0").json())
 print(httpx.get("http://127.0.0.1:8000/items/56").json())
+
+# Pydantic handles API data validation
+print(httpx.get("http://127.0.0.1:8000/items/non_numerus").json())
